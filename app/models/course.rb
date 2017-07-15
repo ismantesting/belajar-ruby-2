e2e7d5896ab74dcba7dcb3f1f389153b60c72b01
ismanuri <ismantesting@gmail.com>
2017-07-15 13:41:29 +0000

    ganti title

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
     has_many :tasks, dependent: :destroy
+    
+    validates :title, presence: true, length: {maximum: 50}
+    validates :description, presence: true
+    validates :price, presence: true, numericality: true
 end
