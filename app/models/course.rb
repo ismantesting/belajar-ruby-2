dea290d99d122572c5e76561b013446c11115f29
ismanuri <ismantesting@gmail.com>
2017-07-15 14:52:06 +0000

    Course and task management

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,7 @@ class Course < ApplicationRecord
     validates :tittle, presence: true, length: {maximum: 50}
     validates :description, presence: true
     validates :price, presence: true, numericality: true
+    
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
