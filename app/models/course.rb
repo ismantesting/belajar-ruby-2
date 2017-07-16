8e7b1fc0ba618a7b4fe1ee2b61d66a155ab6d1ef
ismanuri <ismantesting@gmail.com>
2017-07-16 00:20:28 +0000

    update typo

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     has_many :tasks, dependent: :destroy
     
-    validates :tittle, presence: true, length: {maximum: 50}
+    validates :title, presence: true, length: {maximum: 50}
     validates :description, presence: true
     validates :price, presence: true, numericality: true
     
