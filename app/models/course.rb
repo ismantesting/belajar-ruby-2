e2731fb5479c056c65f08c2fb0e87e7796b76d5e
ismanuri <ismantesting@gmail.com>
2017-07-18 03:16:36 +0000

    update tittle to title

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,6 @@
 class Course < ApplicationRecord
   has_many :tasks, dependent: :destroy
   
-  
   validates :title, presence: true, length: {maximum: 50}
   validates :description, presence: true
   validates :price, presence: true, numericality: true
